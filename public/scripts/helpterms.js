4e6cfe66ea7791f718bb6cb096b0020b868d2c06
Nick Doiron <ndoiron@mapmeld.com>
2013-03-03 18:24:40 +1200

    add more terms to code editor

--- public/scripts/helpterms.js
+++ public/scripts/helpterms.js
@@ -1,5 +1,20 @@
 var helpTerms = [
   { name: "void setup", about: "The robot reads code inside the setup function once, at the start of the program." },
   { name: "void loop", about: "The robot repeats code inside the loop function." },
-  { name: "Serial", about: "The robot uses Serial to send data and other messages back to a computer / the internet." }
+  { name: "Serial begin", about: "Use Serial.begin(9600) to pass messages to the computer and internet" },
+  { name: "Serial println", about: "send a message (number or text) to the computer and internet" },
+  { name: "Serial", about: "The robot uses Serial to send data and other messages back to a computer / the internet." },
+  { name: "pinMode INPUT", about: "Prepares a pin for input from sensors" },
+  { name: "pinMode OUTPUT", about: "Prepares a pin for output to lights, motors, etc" },
+  { name: "pinMode", about: "Prepares a pin for data INPUT or OUTPUT" },
+  { name: "digitalWrite HIGH", about: "Sets a pin to HIGH (on) state" },
+  { name: "digitalWrite LOW", about: "Sets a pin to LOW (off) state" },
+  { name: "digitalWrite", about: "Sets a pin to HIGH or LOW" },
+  { name: "delay", about: "Pauses the program for a time in milliseconds" },
+  { name: "analogRead", about: "Returns sensor value anywhere from 0 (off) to 1024 (100% on)" },
+  { name: "digitalRead", about: "Returns sensor value as HIGH (on) or LOW (off)" },
+  { name: "Servo", about: "Declare a servo motor a the start of code for easier motor control" },
+  { name: "attach", about: "prepares a pin on the Arduino to control a servo motor" },
+  { name: "attach", about: "prepares a pin on the Arduino to control a servo motor" },
+  { name: "write", about: "set a servo motor to a degree between 0 and 179" }
 ];
